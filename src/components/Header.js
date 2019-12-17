af0a01b53302f26b74b34fe97d66c58a0def538c
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2019-12-17 20:52:55 +0500

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Header.css";
 
 const Header = ({ user }) => {
-  return <p>I am {user.name}</p>;
+  const { name, status } = user;
+  return (
+    <header className="Header">
+      <h1 className="Header__name">{name}</h1>
+      <p className="Header__status">{status}</p>
+    </header>
+  );
 };
 
 export default Header;
