98410b7faf29e01a8f187e354297dd92874da686
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2019-12-18 21:46:52 +0500

    chat window and chat messages wired

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import user from "./user";
 import contacts from "./contacts";
 import activeUserId from "./activeUserId";
+import messages from "./messages";
 
 import { combineReducers } from "redux";
 
 export default combineReducers({
   user,
   contacts,
-  activeUserId
+  activeUserId,
+  messages
 });
