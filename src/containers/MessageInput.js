bd5fb3df31179f680dbe275c8fb2816bc1c3af31
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2019-12-20 17:23:16 +0500

    conversation is updating with new message

--- src/containers/MessageInput.js
+++ src/containers/MessageInput.js
@@ -1,15 +1,22 @@
 import React from "react";
 import store from "../store";
-import { setTypingValue } from "../actions";
+import { setTypingValue, sendMessage } from "../actions";
 import "./MessageInput.css";
 
 const MessageInput = ({ value }) => {
+  const state = store.getState();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { typing, activeUserId } = state;
+    store.dispatch(sendMessage(typing, activeUserId));
+  };
   const handleChange = e => {
     store.dispatch(setTypingValue(e.target.value));
   };
 
   return (
-    <form action="" className="Message">
+    <form className="Message" onSubmit={handleSubmit}>
       <input
         className="Message__input"
         onChange={handleChange}
