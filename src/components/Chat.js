24833407d34a499ede27c5a78c9b8759a487a0ea
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2019-12-21 21:24:14 +0500

    WIP: scroll to bottom

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Chat.css";
 
 const Chat = ({ message }) => {
@@ -9,14 +9,29 @@ const Chat = ({ message }) => {
   );
 };
 
-const Chats = ({ messages }) => {
-  return (
-    <div className="Chats">
-      {messages.map(message => (
-        <Chat message={message} key={message.number} />
-      ))}
-    </div>
-  );
-};
+class Chats extends Component {
+  constructor(props) {
+    super(props);
+    this.chatsRef = React.createRef();
+  }
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  scrollToBottom = () => {
+    this.chatsRef.current.scrollTop = this.chatsRef.current.scrollHeight;
+  };
+  render() {
+    return (
+      <div className="Chats" ref={this.chatsRef}>
+        {this.props.messages.map(message => (
+          <Chat message={message} key={message.number} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Chats;
